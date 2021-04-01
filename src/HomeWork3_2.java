3d258e344bb215e40a702d2793097bd1a0ea598a
AliGaraLoc <prezident_alina@mail.ru>
2021-04-02 00:42:14 +0700

    Added home work 3 ex2

---
+++ src/HomeWork3_2.java
@@ -0,0 +1,65 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class HomeWork3_2 {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", "main.MainActivity");
+        capabilities.setCapability("app", "/Users/malinka/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCompareArticleTitle() {
+
+        assertElementHasText(
+                "//*[contains(@text, 'Search Wikipedia')]",
+                "Search Wikipedia",
+                "We see unexpected title"
+        );
+    }
+
+    private WebElement waitForElementPresent(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private void assertElementHasText(String xpath, String expected_title, String error_message) {
+        WebElement element_to_init_search = waitForElementPresent(xpath, "Element not found", 15);
+        String article_title = element_to_init_search.getAttribute("text");
+        Assert.assertEquals(
+                error_message,
+                expected_title,
+                article_title
+        );
+    }
+}
