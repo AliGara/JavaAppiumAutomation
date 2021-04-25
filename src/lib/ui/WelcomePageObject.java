31db54a5eb762cde41792c4ea37d64361146a48c
AliGaraLoc <prezident_alina@mail.ru>
2021-04-25 19:55:03 +0700

    Added changes for ios

---
+++ src/lib/ui/WelcomePageObject.java
@@ -0,0 +1,42 @@
+package lib.ui;
+
+import io.appium.java_client.AppiumDriver;
+
+public class WelcomePageObject extends MainPageObject {
+
+    private static final String
+    STEP_LEARN_MORE_LINK = "id:Learn more about Wikipedia",
+    STEP_NEW_WAY_TO_EXPLORE_TEXT = "id:New ways to explore",
+    STEP_ADD_OR_EDIT_PREFERRED_LANG_TEXT = "id:Add or edit preferred languages",
+    STEP_LEARN_MORE_ABOUT_DATA_COLLECTED = "id:Learn more about data collected",
+    STEP_NEXT_BUTTON = "id:Next",
+    STEP_GET_STARTED_BUTTON = "id:Get started";
+
+    public WelcomePageObject(AppiumDriver driver) {
+        super(driver);
+    }
+
+    public void waitForLearnMoreLink () {
+        this.waitForElementPresent(STEP_LEARN_MORE_LINK, "Cannot find 'Learn more about Wikipedia' link", 10);
+    }
+
+    public void waitForNewWayToExploreText () {
+        this.waitForElementPresent(STEP_NEW_WAY_TO_EXPLORE_TEXT, "Cannot find 'New ways to explore' link", 10);
+    }
+
+    public void waitForAddOrEditPreferredLangText () {
+        this.waitForElementPresent(STEP_ADD_OR_EDIT_PREFERRED_LANG_TEXT, "Cannot find 'Add or edit preferred languages' link", 10);
+    }
+
+    public void waitForLearnMoreAboutDataCollected () {
+        this.waitForElementPresent(STEP_LEARN_MORE_ABOUT_DATA_COLLECTED, "Cannot find 'Learn more about data collected' link", 10);
+    }
+
+    public void clickNextButton () {
+        this.waitForElementAndClick(STEP_NEXT_BUTTON, "Cannot find and click 'Next' button", 10);
+    }
+
+    public void clickGetStartedButton () {
+        this.waitForElementAndClick(STEP_GET_STARTED_BUTTON, "Cannot find and click 'Get started' button", 10);
+    }
+}
